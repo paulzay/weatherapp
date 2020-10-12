44775378ad8bbfaab7bce0095f352cca3213530b
paulzay <paulotieno2@gmail.com>
2020-10-12 18:55:16 +0300

    add webpack and configs

--- src/index.js
+++ src/index.js
@@ -1,21 +1,29 @@
+import './style.css';
+
 const cityForm = document.getElementById('city-form');
 const cityInput = document.getElementById('city');
 
 const weatherResult = (i) => {
   const temp = document.getElementById('temp');
-  const celcius = Math.round(parseFloat(i.main.temp) - 273.15);
+  const celcius = `${Math.round(parseFloat(i.main.temp) - 273.15)} &deg; C `;
+  const fahrenheit = `${Math.round(((parseFloat(i.main.temp) - 273.15) * 1.8) + 32)} &deg; F`;
+
   const { pressure } = i.main;
   const { humidity } = i.main;
   const description = i.weather[0].main;
 
   document.getElementById('description').innerHTML = description;
-  temp.innerHTML = `Temperature: ${celcius} &deg;`;
+  temp.innerHTML = `${celcius}`;
   document.getElementById('pressure').innerHTML = `Pressure: ${pressure}`;
   document.getElementById('humidity').innerHTML = `Humidity: ${humidity}`;
   document.getElementById('location').innerHTML = `Location: ${i.name}`;
 
+  document.getElementById('toggle').addEventListener('click', () => {
+    temp.innerHTML = `${fahrenheit}` ? temp.innerHTML = `${fahrenheit}` : temp.innerHTML = `${celcius}`;
+  });
   return false;
 };
+
 cityForm.addEventListener('submit', (event) => {
   const chosenCity = cityInput.value;
   fetch(
@@ -27,9 +35,8 @@ cityForm.addEventListener('submit', (event) => {
     .then((data) => {
       weatherResult(data);
     })
-    .catch((e) => {
-      console.log(e);
-    });
   event.preventDefault();
   cityForm.reset();
+  document.getElementById('toggle').style.display = 'block';
+  document.querySelector('.titles').style.display = 'block';
 });
